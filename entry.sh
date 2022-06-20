a8ef8a03f23c13ebdfbf186e4f8b2c91f4b4ffcd
Shen Yi Chen <kuya5220@gmail.com>
2022-06-20 16:05:41 +0800

    Update entry script.

--- entry.sh
+++ entry.sh
@@ -1,11 +1,10 @@
 !#/bin/bash
 
-API_KEY=<Input your api key here>
+API_KEY=<your_wandb_api_key>
 EXP_NAME=exp0620
 DATA_SIZE=1
 BATCH_SIZE=128
 EPOCHS=100
 
-cd /LoFi-Loop-Generator
 wandb login $API_KEY
 python train.py --name $EXP_NAME --datasize $DATA_SIZE --batch $BATCH_SIZE --epochs $EPOCHS
